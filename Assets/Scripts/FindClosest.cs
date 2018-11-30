e6ee2ad03c51926295f2883eb6448a590c838758
Unknown <caarroyavepadilla@gmail.com>
2018-11-29 18:27:06 -0600

    undercooked

---
+++ Assets/Scripts/FindClosest.cs
@@ -0,0 +1,33 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindClosest : MonoBehaviour {
+
+	GameObject[] mesas;
+
+	// Update is called once per frame
+	void Update () {
+		FindClosestMesa ();
+	}
+
+	void FindClosestMesa()
+	{
+		float distanceToClosestMesa = Mathf.Infinity;
+		GameObject closetsMesa = null;
+		mesas = GameObject.FindGameObjectsWithTag("mesas");
+
+		foreach (GameObject currentMesa in mesas) {
+			float distanceToMesa = (currentMesa.transform.position - this.transform.position).sqrMagnitude;
+			if (distanceToMesa < distanceToClosestMesa) {
+				distanceToClosestMesa = distanceToMesa;
+				closetsMesa = currentMesa;
+			}
+		}
+
+		Debug.Log(closetsMesa);
+
+		
+	}
+
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	static Animator anim;
+	public float speed = 2.0f;
+	public float rotationspeed = 75.0f;
+
+	// Use this for initialization
+	void Start () {
+		anim  = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float translation = Input.GetAxis("Vertical")*speed;
+		float translation2 = Input.GetAxis("Horizontal")*speed;
+		translation *= Time.deltaTime;
+		translation2 *= Time.deltaTime;
+	
+		Vector3 movimiento = new Vector3(translation2, 0,translation);
+
+		Quaternion rotate = Quaternion.LookRotation(movimiento);
+		rotate *= Quaternion.Euler(0,90,0);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotate,Time.deltaTime*rotationspeed);
+		transform.Translate(translation2,0,translation,Space.World);
+		
+
+		
+
+		if(translation != 0){
+
+			anim.SetBool("isRunning", true);
+		}
+		else if(translation2 != 0){
+			anim.SetBool("isRunning",true);
+		}else{
+			anim.SetBool("isRunning",false);
+
+		}
+	}
+}
